50122ab8330262c3bb11077779b81c1bd2d51238
Daniel Schmidt <dsc@charlietango.dk>
2020-04-11 13:04:49 +0200

    docs: improve storybook example

--- stories/Hooks.story.tsx
+++ stories/Hooks.story.tsx
@@ -4,12 +4,13 @@ import { action } from '@storybook/addon-actions'
 import { IntersectionOptions, useInView } from '../src/index'
 import ScrollWrapper from './ScrollWrapper/index'
 import { CSSProperties } from 'react'
-import { withKnobs, number, boolean } from '@storybook/addon-knobs'
+import { withKnobs, number, boolean, text } from '@storybook/addon-knobs'
 import Status from './Status'
 
 type Props = {
   style?: Object
   children?: React.ReactNode
+  noStatus?: boolean
   options?: IntersectionOptions
 }
 
@@ -22,12 +23,13 @@ const sharedStyle: CSSProperties = {
   textAlign: 'center',
   background: '#148bb4',
   color: 'azure',
+  transition: 'opacity 0.5s',
 }
 
 function getOptions(
   options: IntersectionOptions = { threshold: 0, triggerOnce: false },
 ) {
-  const { threshold, triggerOnce } = options
+  const { threshold, triggerOnce, rootMargin } = options
   return {
     ...options,
     threshold:
@@ -39,11 +41,18 @@ function getOptions(
             max: 1,
             step: 0.1,
           }),
+    rootMargin: text('Root margin', rootMargin || ''),
     triggerOnce: boolean('Trigger once', triggerOnce || false),
   }
 }
 
-const LazyHookComponent = ({ options, style, children, ...rest }: Props) => {
+const LazyHookComponent = ({
+  options,
+  style,
+  children,
+  noStatus,
+  ...rest
+}: Props) => {
   const [ref, inView, entry] = useInView(getOptions(options))
   const [isLoading, setIsLoading] = React.useState(true)
   action('Inview')(inView, entry)
@@ -57,24 +66,51 @@ const LazyHookComponent = ({ options, style, children, ...rest }: Props) => {
   }
 
   return (
-    <div ref={ref} style={{ ...sharedStyle, ...style }} {...rest}>
-      <Status inView={inView} />
+    <div
+      ref={ref}
+      data-inview={inView}
+      style={{
+        ...sharedStyle,
+        ...style,
+        opacity: inView ? 1 : 0.5,
+      }}
+      {...rest}
+    >
+      {!noStatus && <Status inView={inView} />}
       <h2>
-        {children || 'Header is inside the viewport'}: {inView.toString()}
+        {children || 'Inside the viewport'}: {inView.toString()}
       </h2>
     </div>
   )
 }
-const HookComponent = ({ options, style, children, ...rest }: Props) => {
+const HookComponent = ({
+  options,
+  style,
+  children,
+  noStatus,
+  ...rest
+}: Props) => {
   const [ref, inView, entry] = useInView(getOptions(options))
   action('Inview')(inView, entry)
 
   return (
-    <div ref={ref} style={{ ...sharedStyle, ...style }} {...rest}>
-      <Status inView={inView} />
+    <div
+      ref={ref}
+      data-inview={inView}
+      style={{
+        ...sharedStyle,
+        ...style,
+        opacity: inView ? 1 : 0.5,
+      }}
+      {...rest}
+    >
+      {!noStatus && <Status inView={inView} />}
       <h2>
-        {children || 'Header is inside the viewport'}: {inView.toString()}
+        {children || 'Inside the viewport'}: {inView.toString()}
       </h2>
+      <pre style={{ textAlign: 'left' }}>
+        {JSON.stringify(options, null, 2)}
+      </pre>
     </div>
   )
 }
@@ -132,5 +168,53 @@ storiesOf('useInView hook', module)
   .add('Trigger once', () => (
     <ScrollWrapper>
       <HookComponent options={{ triggerOnce: true }} />
+    </ScrollWrapper>
+  ))
+  .add('With root margin', () => (
+    <ScrollWrapper>
+      <HookComponent
+        options={{ rootMargin: '500px 0px' }}
+        style={{ height: 500 }}
+        noStatus
+      />
+      <HookComponent
+        options={{ rootMargin: '500px 0px' }}
+        style={{ height: 500, background: '#4a8f72' }}
+        noStatus
+      />
+      <HookComponent
+        options={{ rootMargin: '500px 0px' }}
+        style={{ height: 500, background: '#4a638f' }}
+        noStatus
+      />
+      <HookComponent
+        options={{ rootMargin: '500px 0px' }}
+        style={{ height: 500, background: '#8f4a62' }}
+        noStatus
+      />
+    </ScrollWrapper>
+  ))
+  .add('Negative root margin', () => (
+    <ScrollWrapper>
+      <HookComponent
+        options={{ rootMargin: '-500px 0px' }}
+        style={{ height: 500 }}
+        noStatus
+      />
+      <HookComponent
+        options={{ rootMargin: '-500px 0px' }}
+        style={{ height: 500, background: '#4a8f72' }}
+        noStatus
+      />
+      <HookComponent
+        options={{ rootMargin: '-500px 0px' }}
+        style={{ height: 500, background: '#4a638f' }}
+        noStatus
+      />
+      <HookComponent
+        options={{ rootMargin: '-500px 0px' }}
+        style={{ height: 500, background: '#8f4a62' }}
+        noStatus
+      />
     </ScrollWrapper>
   ))
